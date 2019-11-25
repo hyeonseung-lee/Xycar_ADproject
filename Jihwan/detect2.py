eca916c2aeb5e00eb249af6bf691db16de4f4783
JIHwan23 <50573418+JIHwan23@users.noreply.github.com>
2019-11-26 00:16:06 +0900

    Update detect2.py

--- Jihwan/detect2.py
+++ Jihwan/detect2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import cv2, time
-import math
 import numpy as np
 
 cap = cv2.VideoCapture("2.avi")
@@ -11,51 +10,35 @@ while True:
     frame = frame[300:340, :]
     blur = cv2.GaussianBlur(frame, (5, 5), 0)
     dst = cv2.Canny(blur, 50, 200, None, 3)
-
     cdst = cv2.cvtColor(dst, cv2.COLOR_GRAY2BGR)
-    cdstP = np.copy(cdst)
 
-    # using Probabilistic Hough Transform
-    lines = cv2.HoughLines(dst, 1, np.pi / 180, 150, None, 0, 0)
-
-    if lines is not None:
-        for i in range(0, len(lines)):
-            rho = lines[i][0][0]
-            theta = lines[i][0][1]
-            a = math.cos(theta)
-            b = math.sin(theta)
-            x0 = a * rho
-            y0 = b * rho
-            pt1 = (int(x0 + 1000 * (-b)), int(y0 + 1000 * (a)))
-            pt2 = (int(x0 - 1000 * (-b)), int(y0 - 1000 * (a)))
-            cv2.line(cdst, pt1, pt2, (0, 0, 255), 3, cv2.LINE_AA)
-
+    # Using Probabilistic Hough Transform
     linesP = cv2.HoughLinesP(dst, 1, np.pi / 180, 50, None, 50, 10)
 
     if linesP is not None:
         for i in range(0, len(linesP)):
             l = linesP[i][0]
-            cv2.line(cdstP, (l[0], l[1]), (l[2], l[3]), (0, 0, 255), 3, cv2.LINE_AA)
+            cv2.line(cdst, (l[0], l[1]), (l[2], l[3]), (0, 0, 255), 3, cv2.LINE_AA)
 
     # Setting range to binary
     lbound = np.array([0, 0, 180], dtype=np.uint8)
     ubound = np.array([131, 255, 255], dtype=np.uint8)
 
     # Convert BGR to HSV
-    hsv = cv2.cvtColor(cdstP, cv2.COLOR_BGR2HSV)
+    hsv = cv2.cvtColor(cdst, cv2.COLOR_BGR2HSV)
 
     # Binary
     bin = cv2.inRange(hsv, lbound, ubound)
 
     # Gray
-    gray = cv2.cvtColor(cdstP, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(cdst, cv2.COLOR_BGR2GRAY)
 
 
     # Original
     cv2.imshow("Source", frame)
 
     # Proccessed by PHT
-    cv2.imshow("PHT", cdstP)
+    cv2.imshow("PHT", cdst)
 
     cv2.imshow("gray", gray)
 
